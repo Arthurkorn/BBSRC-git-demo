03e75b6406834d603c516812ee35eff5d3afdd93
David Mehler <mehlerd@cardiff.ac.uk>
2017-04-10 10:23:09 +0100

    adding assertion test

--- some_code.R
+++ some_code.R
@@ -6,3 +6,12 @@ head(df)
 # R code
 lm.result=lm(conspiracist_avg~age,data=df)
 summary(lm.result)
+# R code
+plot(df$age,df$conspiracist_avg)
+
+# R code
+max_age=120
+min_age=18
+
+stopifnot(max(df$age)<max_age)
+stopifnot(min(df$age)>min_age)
